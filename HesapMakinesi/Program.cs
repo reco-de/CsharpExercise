7fa682a4932654e2231efdcd675d3ea4ef4502fb
Recep Yıldırım <95899401+reco-de@users.noreply.github.com>
2022-12-27 05:01:03 +0300

    Add files via upload

---
+++ HesapMakinesi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Toplama İşleminin Sonucu: " + Toplama(3, 5));
+            Console.WriteLine("Çıkarma İşleminin Sonucu: " + Cikarma(20, 5));
+            Console.WriteLine("Çarpma İşleminin Sonucu: " + Carpma(3, 5));
+            Console.WriteLine("Bölme İşleminin Sonucu: " + Bolme(31, 5));
+        }
+        //Toplama İşlemi
+        static int Toplama(int sayi1 ,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        //Çıkarma İşlemi
+        static int Cikarma(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+        //Çarpma İşlemi
+        static int Carpma(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+        //Bölme İşlemi
+        static float Bolme(float sayi1, float sayi2)
+        {
+            return sayi1 / sayi2;
+        }
+    }
+}
