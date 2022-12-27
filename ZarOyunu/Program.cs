7962b18d6f7c9d79173f85f3b60e2cff8fb920cb
Recep Yıldırım <95899401+reco-de@users.noreply.github.com>
2022-12-27 05:56:22 +0300

    Add files via upload

---
+++ ZarOyunu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ZarOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TavlaZari tavlaZari1 = new TavlaZari(6);
+            TavlaZari tavlaZari2 = new TavlaZari(6);
+            Console.WriteLine("1. Zar: " + tavlaZari1.ZarAt());
+            Console.WriteLine("2. Zar: " + tavlaZari2.ZarAt());
+            
+        }
+    }
+}
---
+++ ZarOyunu/TavlaZari.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZarOyunu
+{
+    class TavlaZari
+    {
+        /// <summary>
+        /// Zarın alabileceği maksimum değer değişkeni
+        /// </summary>
+        int maksimumZarDegeri;
+
+        Random random = new Random();
+
+        /// <summary>
+        /// Zarın alabieceği maksimum değeri giriniz
+        /// </summary>
+        /// <param name="maksimumZarDegeri"></param>
+        public TavlaZari (int maksimumZarDegeri)
+        {
+            this.maksimumZarDegeri = maksimumZarDegeri;
+        }
+
+        /// <summary>
+        /// Rastgele bir sayı için zar at 
+        /// </summary>
+        public int ZarAt ()
+        {
+            //Rastgele sayı üretilecek 
+            return random.Next(1,maksimumZarDegeri +1);
+        }
+    }
+
+
+}
